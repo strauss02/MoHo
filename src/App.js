484aec62d0a7308106412cbdca932cd5a8688672
Ido Strauss <strauss02@hotmail.co.il>
2021-11-21 15:50:19 +0200

    add react router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { Link } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> MoHo</h1>
+      <nav className="border-2 border-red-200">
+        <Link to="/leonardo"> Leonardo </Link> |{' '}
+        <Link to="/orchidea"> Orchidea </Link>
+      </nav>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
