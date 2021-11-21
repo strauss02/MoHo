4f24074c2c71a1e1eb244e58fd16a4c2f177b239
Ido Strauss <strauss02@hotmail.co.il>
2021-11-21 19:31:35 +0200

    add modal

--- src/routes/hotel.jsx
+++ src/routes/hotel.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import { useParams } from "react-router";
+import { Link } from "react-router-dom";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
 
 function Hotel () {
   let params = useParams()
   return (
     <div className=" fixed  top-0 left-0 h-screen w-screen bg-gray-700 bg-opacity-40 z-10 flex justify-center items-center ">
-      <div className="w-5/6 h-5/6 bg-yellow-100"
-      >Hotel {params.hotelName}</div>
+      <div className="w-5/6 h-5/6 bg-yellow-100 flex flex-col items-center rounded-3xl "
+      >
+        <Link 
+        className="ml-auto mr-5 mt-2 text-2xl text-pink-500"
+        to={`/hotels`}
+        >
+        <FontAwesomeIcon  icon={faTimes} />
+        </Link>
+
+        Hotel {params.hotelName}</div>
 
     </div>
   )
